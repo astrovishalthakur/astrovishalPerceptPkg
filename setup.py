e63b8bbf46a80f2b99a153664ac8b9d599b5d2f4
astrovishalthakur <astrovishalthakur@gmail.com>
2022-04-16 19:43:36 +0530

    updated readme file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REPO_NAME = "astrovishalPerceptPkg"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.2",
+    version="0.0.3",
     author= USER_NAME,
     author_email="astrovishalthakur@gmail.com",
     description="A small package for perceptron",
